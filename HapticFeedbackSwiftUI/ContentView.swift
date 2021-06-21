6d00dcebc0d4380a64bbbe4164ec87acc69796a9
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-21 12:53:35 -0300

    Initial Commit

---
+++ HapticFeedbackSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HapticFeedbackSwiftUI
+//
+//  Created by Raphael Cerqueira on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HapticFeedbackSwiftUI/HapticFeedbackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticFeedbackSwiftUIApp.swift
+//  HapticFeedbackSwiftUI
+//
+//  Created by Raphael Cerqueira on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HapticFeedbackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

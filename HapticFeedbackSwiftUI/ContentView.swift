c2e45f0ad8dd9c551971230a8a66b4b1e90bf796
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-22 05:39:24 -0300

    Primeiro commit

--- HapticFeedbackSwiftUI/ContentView.swift
+++ HapticFeedbackSwiftUI/ContentView.swift
@@ -7,10 +7,91 @@
 
 import SwiftUI
 
+// pt-BR
+// Para testar o retorno tátil é necessário rodar no aparelho
+
+// en-US
+// In order to test the haptic feedback you should run it on the device
+
+// That's it
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header: Text("selection")) {
+                    Text("selection")
+                        .onTapGesture {
+                            performSelectionFeedback()
+                        }
+                }
+                
+                Section(header: Text("impact")) {
+                    Text("light")
+                        .onTapGesture {
+                            performImpactFeedback(style: .light)
+                        }
+                    
+                    Text("medium")
+                        .onTapGesture {
+                            performImpactFeedback(style: .medium)
+                        }
+                    
+                    Text("heavy")
+                        .onTapGesture {
+                            performImpactFeedback(style: .heavy)
+                        }
+                    
+                    Text("soft")
+                        .onTapGesture {
+                            performImpactFeedback(style: .soft)
+                        }
+                    
+                    Text("rigid")
+                        .onTapGesture {
+                            performImpactFeedback(style: .rigid)
+                        }
+                    
+                }
+                
+                Section(header: Text("notification")) {
+                    Text("success")
+                        .onTapGesture {
+                            performNotificationFeedback(type: .success)
+                        }
+                    
+                    Text("warning")
+                        .onTapGesture {
+                            performNotificationFeedback(type: .warning)
+                        }
+                    
+                    Text("error")
+                        .onTapGesture {
+                            performNotificationFeedback(type: .error)
+                        }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Haptic Feedback")
+        }
+    }
+    
+    func performSelectionFeedback() {
+        print("selection")
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
+    
+    func performImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        print("impact")
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+    
+    func performNotificationFeedback(type: UINotificationFeedbackGenerator.FeedbackType) {
+        print("notification")
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,23 @@
+# Feedback tátil usando SwiftUI
+Implementação de como usar o retorno tátil do aparelho. 
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/122835994-c2980680-d2c7-11eb-9c67-4cb7edc73851.png" width="400">    
+</p>
+
+## Vídeos
+- [Vídeo no Youtube](https://youtu.be/R4dkFuDSTlM)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
